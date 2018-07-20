d2537221ea50266cce40f089a94c296dec3703e6
viher3 <viher_3@hotmail.com>
2018-07-20 20:19:56 +0200

    ReaccionCmsBundle init

---
+++ ReaccionCmsBundle.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+ * This file is part of the ReaccionCmsBundle package.
+ *
+ * (c) Reaccion Estudio <https://www.reaccionestudio.com/>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace ReaccionEstudio\ReaccionCms;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * ReaccionCmsBundle class.
+ *
+ * @author      Alberto Vian <alberto@reaccionestudio.com>
+ */
+class ReaccionCmsBundle extends Bundle
+{
+}
