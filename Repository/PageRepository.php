1c38fb6e7fae7d2e26dfb25976b8a5e9a74d486a
viher3 <viher_3@hotmail.com>
2018-08-03 04:01:21 +0200

    Fixed PageRepository

--- Repository/PageRepository.php
+++ Repository/PageRepository.php
@@ -1,8 +1,15 @@
 <?php
 
-	namespace App\ReaccionEstudio\ReaccionCMSBundle\Repository;
+namespace App\ReaccionEstudio\ReaccionCMSBundle\Repository;
 
-	class PageRepository
-	{
+use App\ReaccionEstudio\ReaccionCMSBundle\Entity\Page;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Symfony\Bridge\Doctrine\RegistryInterface;
 
-	}
+class PageRepository extends ServiceEntityRepository
+{
+	public function __construct(RegistryInterface $registry)
+    {
+        parent::__construct($registry, Page::class);
+    }
+}
