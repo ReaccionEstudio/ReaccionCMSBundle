f043477db8483e1afbe0ae82f837d6a5f0f2fd23
viher3 <viher_3@hotmail.com>
2018-11-07 17:34:44 +0100

    Image sizes

--- Resources/init_data/install.sql
+++ Resources/init_data/install.sql
@@ -1,1 +1,6 @@
-# TODO
+# Config
+INSERT INTO config (name,value) VALUES 
+	('small.image.size','a:2:{s:6:"height";i:150;s:5:"width";i:150;}'),
+	('medium.image.size','a:2:{s:6:"height";i:800;s:5:"width";i:600;}'),
+	('large.image.size','a:2:{s:6:"height";i:1600;s:5:"width";i:1200;}')
+;
