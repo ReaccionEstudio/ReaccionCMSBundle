0bda22d627433797506fdd1291338cad72ec7431
viher3 <viher_3@hotmail.com>
2018-11-07 16:50:09 +0100

    Configuration Entity

---
+++ Entity/Configuration.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\ReaccionEstudio\ReaccionCMSBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Configuration
+ *
+ * @ORM\Table(name="config")
+ * @ORM\Entity(repositoryClass="App\ReaccionEstudio\ReaccionCMSBundle\Repository\ConfigurationRepository")
+ */
+class Configuration
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="value", type="text")
+     */
+    private $value;
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     *
+     * @return self
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     *
+     * @return self
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getValue()
+    {
+        return $this->value;
+    }
+
+    /**
+     * @param string $value
+     *
+     * @return self
+     */
+    public function setValue($value)
+    {
+        $this->value = $value;
+
+        return $this;
+    }
+}
---
+++ Repository/ConfigurationRepository.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\ReaccionEstudio\ReaccionCMSBundle\Repository;
+
+use App\ReaccionEstudio\ReaccionCMSBundle\Entity\Configuration;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Symfony\Bridge\Doctrine\RegistryInterface;
+
+class ConfigurationRepository extends ServiceEntityRepository
+{
+	public function __construct(RegistryInterface $registry)
+    {
+        parent::__construct($registry, PageContent::class);
+    }
+}
