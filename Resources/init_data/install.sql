8bbbd4e7bb5e70c20b7866c3dd2c2d577022c629
viher3 <viher_3@hotmail.com>
2018-11-19 02:18:23 +0100

    Added 'current_theme' config parameter

--- Resources/init_data/install.sql
+++ Resources/init_data/install.sql
@@ -4,5 +4,6 @@ INSERT INTO config (name,value,type,required) VALUES
 	('medium.image.size','a:2:{s:6:"height";i:800;s:5:"width";i:600;}','serialized',1),
 	('large.image.size','a:2:{s:6:"height";i:1600;s:5:"width";i:1200;}','serialized',1),
 	('language','en',null,1),
-	('themes_path','templates/ReaccionCMSBundle/themes/',null,1)
+	('themes_path','templates/ReaccionCMSBundle/themes/',null,1),
+	('current_theme','rocket_theme',null,1)
 ;
