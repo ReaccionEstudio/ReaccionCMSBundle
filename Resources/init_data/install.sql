9f9ced0e97b653c86a25496410f89cebb089a8ce
viher3 <viher_3@hotmail.com>
2018-11-19 01:36:07 +0100

    Added 'themes_path' configuration value in install.sql file

--- Resources/init_data/install.sql
+++ Resources/init_data/install.sql
@@ -3,5 +3,6 @@ INSERT INTO config (name,value,type,required) VALUES
 	('small.image.size','a:2:{s:6:"height";i:150;s:5:"width";i:150;}','serialized',1),
 	('medium.image.size','a:2:{s:6:"height";i:800;s:5:"width";i:600;}','serialized',1),
 	('large.image.size','a:2:{s:6:"height";i:1600;s:5:"width";i:1200;}','serialized',1),
-	('language','en',null,1)
+	('language','en',null,1),
+	('themes_path','templates/ReaccionCMSBundle/themes/',null,1)
 ;
