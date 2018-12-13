988b859bbf721a926aa3ff700ecd0bab1726e547
viher3 <viher_3@hotmail.com>
2018-12-13 19:23:05 +0100

    HtmlAttributesHelper class init

---
+++ Helpers/HtmlAttributesHelper.php
@@ -0,0 +1,18 @@
+<?php
+
+	namespace App\ReaccionEstudio\ReaccionCMSBundle\Heleprs;
+
+	final class HtmlAttributesHelper
+	{
+		private $attrs;
+
+		public function __construct(Array $attrs)
+		{
+			$this->attrs = $attrs;
+		}
+
+		public function getAttributesAsString()
+		{
+			// TODO
+		}
+	}
