bb14a3e5cfe772663205a9c050325191efe67ae2
viher3 <viher_3@hotmail.com>
2018-12-15 16:56:53 +0100

    Created 'entries_resume_words_length' in install.sql file

--- Resources/init_data/install.sql
+++ Resources/init_data/install.sql
@@ -4,9 +4,10 @@ INSERT INTO config (name,value,type,required) VALUES
 	('medium.image.size','a:2:{s:6:"height";i:800;s:5:"width";i:600;}','serialized',1),
 	('large.image.size','a:2:{s:6:"height";i:1600;s:5:"width";i:1200;}','serialized',1),
 	('language','en',null,1),
-	('themes_path','templates/ReaccionCMSBundle/themes/',null,1),
-	('current_theme','rocket_theme',null,1),
-	('site_name','ReaccionCMS',null,1),
+	('themes_path','templates/ReaccionCMSBundle/themes/','text',1),
+	('current_theme','rocket_theme','text',1),
+	('site_name','ReaccionCMS','text',1),
 	('admin_logo','images/reaccion_cms_logo.jpg',null,1),
-	('entries_list_pagination_limit',10,null,1)
+	('entries_list_pagination_limit',10,'number',1),
+	('entries_resume_words_length', 30,'number',1)
 ;
