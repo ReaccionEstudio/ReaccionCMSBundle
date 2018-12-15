4fdea08a5c821813f4b667d4c0064ae6a70f6b1b
viher3 <viher_3@hotmail.com>
2018-12-15 01:44:08 +0100

    Defined EntryService

---
+++ Services/Entries/EntryService.php
@@ -0,0 +1,21 @@
+<?php
+
+	namespace App\ReaccionEstudio\ReaccionCMSBundle\Services\Entries;
+
+	use Doctrine\ORM\EntityManagerInterface;
+
+	/**
+	 * Entry service.
+	 *
+	 * @author Alberto Vian <alberto@reaccionestudio.com>
+	 */
+	class EntryService
+	{
+		/**
+		 * Load entries for current page lang
+		 */
+		public function loadEntries(String $language = "en")
+		{
+			// TODO
+		}
+	}
