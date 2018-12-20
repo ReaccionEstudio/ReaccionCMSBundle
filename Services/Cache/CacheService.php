70aee21bad9a1f720d42628ad986de67213a3477
viher3 <viher_3@hotmail.com>
2018-12-20 13:13:13 +0100

    Created remove, hasItem and searchKey methods in Cache service

--- Services/Cache/CacheService.php
+++ Services/Cache/CacheService.php
@@ -72,8 +72,54 @@
 			return null;
 		}
 
-		public function remove(String $key)
+		/**
+		 * Search cache item by key
+		 * 
+		 * @param 	String 		$query 		Search query
+		 * @return 	Array 		$results 	Search results
+		 */
+		public function searchKey(String $query = "") : Array
 		{
+			if( ! strlen($query) ) return [];
+
+			$iter 	 = new \APCIterator('user');
+			$results = [];
+
+			foreach ($iter as $item) 
+			{
+				if(preg_match("/" . $query . "/", $item['key']))
+				{
+					$results[] = $item['value'];
+				}
+			}
+
+			return $results;
+		}
+
+		/**
+		 * Check if item exists in cache storage
+		 * 
+		 * @param 	String 	$key 	Cache item key
+		 * @return 	Array 	[type] 	Cache item value
+		 */
+		public function hasItem(String $key)
+		{
+			return $this->cache->hasItem($key);
+		}
+
+		/**
+		 * Remove item from cache storage
+		 *
+		 * @param 	String 		$key 		Cache item key
+		 * @return 	Boolean		true|false 	Operation result
+		 */
+		public function remove(String $key) : bool
+		{
+			if($this->cache->hasItem($key))
+			{
+				return $this->cache->deleteItem($key);
+			}
 			
+			return false;
 		}
 	}
