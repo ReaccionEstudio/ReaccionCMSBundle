d0f20c3136b2fe60adc921b0a10af42106a6b436
viher3 <viher_3@hotmail.com>
2018-12-23 01:04:43 +0100

    Page type field setted nullable

--- Entity/Page.php
+++ Entity/Page.php
@@ -46,7 +46,7 @@ class Page
     /**
      * @var string
      *
-     * @ORM\Column(name="type", type="string", length=255)
+     * @ORM\Column(name="type", type="string", length=255, nullable=true)
      */
     private $type;
 
