d2164d5f8f78f1295fd9b12af9d2c6c4dfe20994
viher3 <viher_3@hotmail.com>
2018-12-23 14:15:22 +0100

    Created getEntries() entity repository method

--- Repository/EntryRepository.php
+++ Repository/EntryRepository.php
@@ -12,4 +12,33 @@ class EntryRepository extends ServiceEntityRepository
     {
         parent::__construct($registry, Entry::class);
     }
+
+    /**
+     * Get entry entities
+     *
+     * @param 	Array 	$filters 	Query filters
+     * @return 	Array 	[type]		Query result
+     */
+    public function getEntries(Array $filters = [])
+    {
+    	$defaultFilters = ['language' => 'en'];
+    	$filters = array_merge($defaultFilters, $filters);
+
+    	// query builder
+    	$qb = $this->createQueryBuilder('e')
+    				->where('e.enabled = 1')
+    				->andWhere('e.language = :language');
+
+    	if(isset($filters['categories']))
+    	{
+    		$qb->innerJoin('e.categories', 'c')
+           	   ->andWhere('c.slug IN (:categorySlugs)')
+           	   ->setParameter('categorySlugs', $filters['categories']);
+    	}
+
+    	$qb->orderBy('e.id', 'DESC');
+    	$qb->setParameter('language', $filters['language']);
+
+        return $qb->getQuery()->getResult();
+    }
 }
