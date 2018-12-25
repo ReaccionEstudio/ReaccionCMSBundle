acc37fd4aa0aa1d45dbefb8025803021f4dc57d6
viher3 <viher_3@hotmail.com>
2018-12-25 23:18:06 +0100

    Created userExists() method in UserRepository class

--- Repository/UserRepository.php
+++ Repository/UserRepository.php
@@ -2,9 +2,9 @@
 
 namespace App\ReaccionEstudio\ReaccionCMSBundle\Repository;
 
-use App\Entity\User;
-use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Symfony\Bridge\Doctrine\RegistryInterface;
+use App\ReaccionEstudio\ReaccionCMSBundle\Entity\User;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 
 /**
  * @method User|null find($id, $lockMode = null, $lockVersion = null)
@@ -17,5 +17,28 @@ class UserRepository extends ServiceEntityRepository
     public function __construct(RegistryInterface $registry)
     {
         parent::__construct($registry, User::class);
+    }
+
+    /**
+	 * Check if username and email already exists
+	 * 
+	 * @param 	String 	$username 	Username
+	 * @param 	String 	$email 		Email
+	 * @return 	Array   [type] 	    Found user data
+	 */
+    public function userExists(String $username, String $email)
+    {
+    	$em = $this->getEntityManager();
+
+    	$dql = "SELECT COUNT(u.id) AS total, u.usernameCanonical, u.emailCanonical 
+                FROM App\ReaccionEstudio\ReaccionCMSBundle\Entity\User u 
+                WHERE u.usernameCanonical = :username 
+                OR  u.emailCanonical = :email";
+
+        $query = $em->createQuery($dql)
+        			 ->setParameter('username', $username)
+        			 ->setParameter('email', $email);
+
+    	return $query->getSingleResult();
     }
 }
