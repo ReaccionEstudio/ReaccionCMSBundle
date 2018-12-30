064d81b80c498545a1eb87973e538cc40d2e1b2b
viher3 <viher_3@hotmail.com>
2018-12-31 00:57:06 +0100

    Created comment sanitizer class

---
+++ Services/Comment/CommentSanitizer.php
@@ -0,0 +1,46 @@
+<?php
+
+	namespace App\ReaccionEstudio\ReaccionCMSBundle\Services\Comment;
+
+	/**
+	 * Formats and sanitizes the comment content.
+	 *
+	 * @author Alberto Vian <alberto@reaccionestudio.com>
+	 */
+	final class CommentSanitizer
+	{
+		/**
+		 * @var String
+		 *
+		 * Comment content
+		 */
+		private $content;
+
+		/**
+		 * Constructor
+		 */
+		public function __construct(String $content)
+		{
+			$this->content = $content;
+			$this->sanitize();
+		}
+
+		/**
+		 * Sanitize comment content
+		 */
+		public function sanitize() : void
+		{
+			$this->content = nl2br($this->content);
+			$this->content = strip_tags($this->content, "<br>");
+		}
+
+		/**
+		 * Get content var value
+		 *
+		 * @return String 	$this->content 	Sanitized comment content
+		 */
+		public function getContent() : String
+		{
+			return $this->content;
+		}
+	}
