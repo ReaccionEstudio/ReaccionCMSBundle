602d89a225e6f877c8eeea01c76f02642fd6eee5
viher3 <viher_3@hotmail.com>
2018-12-31 23:39:07 +0100

    Created RoutingManager js class

---
+++ Resources/public/javascript/RoutingManager.js
@@ -0,0 +1,38 @@
+// fos-js-routing
+const routes = require('../../../../../../public/js/fos_js_routes.json');
+import Routing from '../../../../../../vendor/friendsofsymfony/jsrouting-bundle/Resources/public/js/router.min.js';
+
+Routing.setRoutingData(routes);
+
+/**
+ * Routing manager class
+ *
+ * @author  Alberto Vian - alberto@reaccionestudio.com
+ * @website reaccionestudio.com
+ */
+class RoutingManager
+{
+	/**
+   	* Constructor
+   	*/
+	constructor(){ }
+
+	/**
+	 * Generate route
+	 *
+	 * @param  String 	route 	Route name
+	 * @param  JSON 	params 	Route params
+	 * @return String 	[type] 	Route url
+	 */
+	generate(route, params)
+	{
+		if(typeof params == "undefined")
+		{
+			params = {};
+		}
+
+		return Routing.generate(route, params);
+	}
+}
+
+export default RoutingManager;
