a229457487ebd5a6e96042104b39ac451eaf574d
viher3 <viher_3@hotmail.com>
2019-01-02 00:30:43 +0100

    Added write_reply_textarea_placeholder js translation

--- Resources/public/javascript/translations/Messages.js
+++ Resources/public/javascript/translations/Messages.js
@@ -1,7 +1,8 @@
 export default 
 {
 	"en" : {
-		"remove_comment_confirmation" : "Are you sure you want to delete this comment?"
+		"remove_comment_confirmation" : "Are you sure you want to delete this comment?",
+		"write_reply_textarea_placeholder" : "Write your reply here ..."
 	},
 	"es" : {}
 }
