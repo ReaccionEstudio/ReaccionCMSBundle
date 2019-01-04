024e5c70cefae19e5c345371be4010764dc28ce1
viher3 <viher_3@hotmail.com>
2019-01-04 19:56:18 +0100

    Created cache constant file

---
+++ Constants/Cache.php
@@ -0,0 +1,15 @@
+<?php
+	
+	namespace App\ReaccionEstudio\ReaccionCMSBundle\Constants;
+
+	/**
+	 * Cache constants
+	 */
+	class Cache
+	{
+		const ITEMS = [
+
+			'main_page' => 'main_page'
+
+		];
+	}
