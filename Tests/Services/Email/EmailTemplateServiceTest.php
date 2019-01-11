6bdaf6f8fdfa9cfb9c9754ef3a893453290e0ffb
viher3 <viher_3@hotmail.com>
2019-01-11 16:08:21 +0100

    Created EmailTemplate service test

---
+++ Tests/Services/Email/EmailTemplateServiceTest.php
@@ -0,0 +1,32 @@
+<?php
+	
+	namespace App\ReaccionEstudio\ReaccionCMSBundle\Tests\Services\Email;
+	
+	use PHPUnit\Framework\TestCase;
+	use Doctrine\Common\Collections\ArrayCollection;
+	use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+	use App\ReaccionEstudio\ReaccionCMSBundle\Entity\EmailTemplate;
+
+	/**
+	 * Email template service test
+	 *
+	 * @author Alberto Vian <alberto@reaccionestudio.com>
+	 */
+	class EmailTemplateServiceTest extends KernelTestCase
+	{
+		private $emailTemplateService;
+
+		public function setUp()
+	    {
+	    	$kernel = self::bootKernel();
+	    	$this->emailTemplateService = $kernel->getContainer()->get('reaccion_cms.email_template');
+	    }
+
+	    public function testLoadTemplate()
+	    {
+	    	$this->emailTemplateService->loadTemplate('test-email');
+	    	$bodyHtml = $this->emailTemplateService->getBodyHtml();
+
+	    	$this->assertNotEmpty($bodyHtml);
+	    }
+	}
