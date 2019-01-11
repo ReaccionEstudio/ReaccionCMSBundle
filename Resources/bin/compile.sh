eb4a8722a23443f1edc753feb6ebfe604d35a084
viher3 <viher_3@hotmail.com>
2019-01-11 16:09:58 +0100

    Fixed path error in compile.sh script

--- Resources/bin/compile.sh
+++ Resources/bin/compile.sh
@@ -15,7 +15,7 @@ cp -R ${REACCION_ADMIN_BUNDLES_TEMPLATES_PATH} ${BUNDLES_TEMPLATES_PATH}/bundles
 
 # copying ReaccionCMS/Resources/views/emailTemplates
 echo "Copying ${REACCION_TEMPLATES_PATH}/emailTemplates in ${BUNDLES_TEMPLATES_PATH}/ReaccionCMSBundle/emailTemplates ..."
-cp -R ${REACCION_TEMPLATES_PATH}/emailTemplates ${BUNDLES_TEMPLATES_PATH}/ReaccionCMSBundle/emailTemplates
+cp -R ${REACCION_TEMPLATES_PATH}/emailTemplates ${BUNDLES_TEMPLATES_PATH}/ReaccionCMSBundle
 
 # copying ReaccionCMS/Resources/views/rocket_theme
 echo "Copying ${REACCION_TEMPLATES_PATH}/themes/rocket_theme in ${BUNDLES_TEMPLATES_PATH}/ReaccionCMSBundle/rocket_theme ..."
