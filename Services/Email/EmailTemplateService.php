4d4244c357416f1c7d1ca861809f08a0247a9fca
viher3 <viher_3@hotmail.com>
2019-01-11 16:09:37 +0100

    Created Email template service

---
+++ Services/Email/EmailTemplateService.php
@@ -0,0 +1,220 @@
+<?php
+
+	namespace App\ReaccionEstudio\ReaccionCMSBundle\Services\Email;
+
+	use Doctrine\ORM\EntityManagerInterface;
+	use App\ReaccionEstudio\ReaccionCMSBundle\Entity\EmailTemplate;
+	use App\ReaccionEstudio\ReaccionCMSBundle\Services\Utils\LoggerService;
+	use App\ReaccionEstudio\ReaccionCMSBundle\Services\Config\ConfigService;
+	use App\ReaccionEstudio\ReaccionCMSBundle\Services\Language\LanguageService;
+
+	/**
+	 * Email template service.
+	 *
+	 * @author Alberto Vian <alberto@reaccionestudio.com>
+	 */
+	class EmailTemplateService
+	{
+		/**
+		 * @var EntityManagerInterface
+		 *
+		 * EntityManager
+		 */
+		private $em;
+
+		/**
+		 * @var LoggerService
+		 *
+		 * Logger
+		 */
+		private $logger;
+
+		/**
+		 * @var Array
+		 *
+		 * Email template data
+		 */
+		private $emailTemplateData = [];
+
+		/**
+		 * @var String
+		 *
+		 * Email body HTML
+		 */
+		private $bodyHtml = '';
+
+		/**
+		 * @var Twig_Environment
+		 *
+		 * Twig
+		 */
+		private $twig;
+
+		/**
+		 * @var ConfigService
+		 *
+		 * Config
+		 */
+		private $config;
+
+		/**
+		 * Constructor
+		 */
+		public function __construct(EntityManagerInterface $em, LoggerService $logger, LanguageService $language, \Twig_Environment $twig, ConfigService $config, String $projectDir)
+		{
+			$this->em 				= $em;
+			$this->twig 	 		= $twig;
+			$this->logger 			= $logger;
+			$this->config 			= $config;
+			$this->language 		= $language;
+			$this->projectDir 		= $projectDir;
+			$this->currentLanguage 	= $this->language->getLanguage();
+		}
+
+		/**
+		 * Get email body HTML
+		 *
+		 * @return String 	$this->bodyHtml 	Email body HTML
+		 */
+		public function getBodyHtml() : String
+		{
+			return $this->bodyHtml;
+		}
+
+		/**
+		 * Get email template data array
+		 *
+		 * @return Array 	$this->emailTemplateData 	Email template data
+		 */
+		public function getEmailTemplateData() : Array
+		{
+			return $this->emailTemplateData;
+		}
+
+		/**
+		 * Load email template data
+		 *
+		 * @param  String 		$slug 				EmailTemplate slug
+		 * @return self 		[type] 		
+		 */
+		public function loadTemplate(String $slug) : self
+		{
+			$emailTemplateEntity = $this->loadTemplateEntity($slug);
+			
+			if(empty($emailTemplateEntity) && $this->currentLanguage != "en")
+			{
+				$emailTemplateEntity = $this->loadTemplateEntity($slug, "en");
+			}
+
+			if(empty($emailTemplateEntity))
+			{
+				$this->logger->addError("Email template with slug '" . $slug . "' and language '[" . $this->currentLanguage . ", en]' not found.");
+				return $this;
+			}
+
+			$this->emailTemplateData = [
+				'name' => $emailTemplateEntity->getName(),
+				'message' => $emailTemplateEntity->getMessage(),
+				'subject' => $emailTemplateEntity->getSubject(),
+				'fromname' => $emailTemplateEntity->getFromname(),
+				'fromemail' => $emailTemplateEntity->getFromemail(),
+				'template_file' => $emailTemplateEntity->getTemplateFile(),
+				'plain_text' => $emailTemplateEntity->isPlainText(),
+				'enabled' => $emailTemplateEntity->isEnabled(),
+				'custom_params' => json_decode($emailTemplateEntity->getMessageParams(), true)
+			];
+
+			$this->generateEmailHtml();
+
+			return $this;
+		}
+
+		/**
+		 * Generate email body HTML
+		 */
+		private function generateEmailHtml() : void
+		{
+			$emailFileHtml = $this->getTemplateFileHtml($this->emailTemplateData['template_file']);
+
+			if( ! empty($this->emailTemplateData['custom_params']))
+			{
+				foreach($this->emailTemplateData['custom_params'] as $param)
+				{
+					if( ! preg_match("/" . $param['name'] . "/", $emailFileHtml)) continue;
+					
+					$emailFileHtml = str_replace($param['name'], $param['value'], $emailFileHtml);
+				}
+			}
+
+			$this->bodyHtml = $emailFileHtml;
+		}
+
+		/**
+		 * Get template file HTML code
+		 *
+		 * @param  String 	$file 				Template view filename
+		 * @return String 	$emailFileHtml 		Email view file HTML
+		 */
+		private function getTemplateFileHtml(String $file) : String
+		{
+			$emailFileHtml = "";
+
+			// get full email template
+			$emailTemplatesPath = $this->projectDir;
+			$emailTemplatesPath .= "/" . $this->config->get("email_templates_path") . $file;
+
+			if( ! file_exists($emailTemplatesPath) )
+			{
+				$this->logger->addError("Email template view file '" . $emailTemplatesPath . "' not found.");
+				return '';
+			}
+
+			// get relative email template
+			$relativeEmailTemplatesPath = explode("templates/", $emailTemplatesPath);
+			$relativeEmailTemplatesPath = $relativeEmailTemplatesPath[1];
+
+			// get email file html
+			$emailFileHtml = $this->twig->render($relativeEmailTemplatesPath);
+
+			return $emailFileHtml;
+		}
+
+		/**
+		 * Replace custom parameters defined in the message with their value
+		 *
+		 * @param  EmailTemplate 	$emailTemplateEntity 	Email template entity
+		 * @return String 			$message 				Final email template message
+		 */
+		private function replaceMessageCustomParams(EmailTemplate $emailTemplateEntity) : String
+		{
+			$message = $emailTemplateEntity->getMessage();
+			$customParams = $emailTemplateEntity->getMessageParams();
+			$customParams = json_decode($customParams);
+
+			if(empty($customParams)) return $message;
+
+			foreach($customParams as $param)
+			{
+				if( ! preg_match("/" . $param . "/", $message)) continue;
+
+				$message = str_replace($param['name'], $param['value'], $message);
+			}
+
+			return $message;
+		}
+
+		/**
+		 * Get EmailTemplate entity from database
+		 *
+		 * @param  String 				$slug 		EmailTemplate slug
+		 * @param  String 				$language 	EmailTemplate language
+		 * @return EmailTemplate|null 	[type]		EmailTemplate entity
+		 */
+		private function loadTemplateEntity(String $slug, String $language = "")
+		{
+			$language = (strlen($language)) ? $language : $this->currentLanguage;
+
+			$entityFilters = [ 'slug' => $slug, 'language' => $language ];
+			return $this->em->getRepository(EmailTemplate::class)->findOneBy($entityFilters);
+		}
+	}
