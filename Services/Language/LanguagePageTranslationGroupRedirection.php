b46b837ce727ee108a523d7cba46c02d3622b9c3
viher3 <viher_3@hotmail.com>
2019-01-20 02:03:28 +0100

    Created redirectByTranslationGroup() method in LanguagePageTranslationGroupRedirection class

--- Services/Language/LanguagePageTranslationGroupRedirection.php
+++ Services/Language/LanguagePageTranslationGroupRedirection.php
@@ -3,7 +3,9 @@
 	namespace App\ReaccionEstudio\ReaccionCMSBundle\Services\Language;
 
 	use Doctrine\ORM\EntityManagerInterface;
+	use Symfony\Component\HttpFoundation\RedirectResponse;
 	use App\ReaccionEstudio\ReaccionCMSBundle\Entity\Page;
+	use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 	use App\ReaccionEstudio\ReaccionCMSBundle\Entity\PageTranslationGroup;
 	use App\ReaccionEstudio\ReaccionCMSAdminBundle\Constants\Languages;
 
@@ -22,20 +24,77 @@
 		private $em;
 
 		/**
+	     * @var UrlGeneratorInterface
+	     */
+	    protected $router;
+
+		/**
 		 * Constructor
 		 */
-		public function __construct(EntityManagerInterface $em)
+		public function __construct(EntityManagerInterface $em, UrlGeneratorInterface $router)
 		{
 			$this->em = $em;
+			$this->router = $router;
 		}
 
 		/**
 		 * Redirect by page translation group for the given language
 		 */
-		public function redirectByTranslationGroup(String $language, String $route, String $routeSlug="")
+		public function redirectByTranslationGroup(String $language, String $refererUrl, String $route, String $routeSlug="")
 		{
-			// TODO ...
-			var_dump($route);
-			var_dump($routeSlug);
+			if($route == "index_slug")
+			{
+				// find page entity by slug
+				$redirectionUrl = $this->findPageEntityBySlug($language, $routeSlug);
+			}
+			else if($route == "index")
+			{
+				// get main page entity for given language
+				$mainPageEntity = $this->getMainTranslationPageEntity();
+				$redirectionUrl = $this->router->generate('index_slug', [ 'slug' => $mainPageEntity->getSlug() ]);
+			}
+			else
+			{
+				// redirect to the referer url
+				$redirectionUrl = $refererUrl;
+			}
+			
+			return new RedirectResponse($redirectionUrl);
+		}
+
+		/**
+		 * Find page entity by slug for given language
+		 *
+		 * @param  String 	$language 	Page language
+		 * @param  String 	$routeSlug	Referer route slug
+		 * @return String 	[type]		Translation page url
+		 */
+		private function findPageEntityBySlug(String $language, String $routeSlug) : String
+		{
+			$currentPageEntity = $this->em->getRepository(Page::class)->findOneBy(['slug' => $routeSlug ]);
+			$translationGroup  = $currentPageEntity->getTranslationGroup();
+
+			$pageFilters = [ 'translationGroup' => $translationGroup, 'language' => $language ];
+			$pageEntity  = $this->em->getRepository(Page::class)->findOneBy($pageFilters);
+
+			if( empty($pageEntity) )
+			{
+				// redirect to language main page
+				$pageEntity = $this->getMainTranslationPageEntity($language);
+			}
+
+			return $this->router->generate('index_slug', [ 'slug' => $pageEntity->getSlug() ]);
+		}
+
+		/**
+		 * Get the main page for given translation
+		 *
+		 * @param  String 		$language 	Page language
+		 * @return Page|null 	[type]		Main page entity
+		 */
+		private function getMainTranslationPageEntity(String $language)
+		{
+			$filters = [ 'language' => $language, 'mainPage' => true ];
+			return $this->em->getRepository(Page::class)->findOneBy($filters);
 		}
 	}
