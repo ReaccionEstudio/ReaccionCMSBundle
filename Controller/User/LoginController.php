d886166f77468d64686ba47081d29518a0d2b0bb
viher3 <viher_3@hotmail.com>
2019-01-24 23:33:43 +0100

    User controller refactorized in multiple controllers

--- Controller/User/LoginController.php
+++ Controller/User/LoginController.php
@@ -1,6 +1,6 @@
 <?php
 
-	namespace App\ReaccionEstudio\ReaccionCMSBundle\Controller;
+	namespace App\ReaccionEstudio\ReaccionCMSBundle\Controller\User;
 
 	use Symfony\Component\HttpFoundation\Request;
 	use Symfony\Component\Translation\TranslatorInterface;
@@ -13,56 +13,12 @@
 	use Symfony\Component\Security\Http\Event\InteractiveLoginEvent;
 	use Symfony\Component\Security\Core\Encoder\EncoderFactoryInterface;
 
-	class UserController extends Controller
+	class LoginController extends Controller
 	{
-		/**
-		 * Register a new user
-		 */
-		public function register(Request $request, TranslatorInterface $translator)
-		{
-			$sitename = $this->get("reaccion_cms.config")->get("site_name");
-			$seo = [
-				'title' => $translator->trans("user_register.seo_title", ['%sitename%' => $sitename])
-			];
-
-			// form
-			$form = $this->createForm(UserRegisterType::class);
-			$form->handleRequest($request);
-
-			if ($form->isSubmitted() && $form->isValid()) 
-			{
-				$username 	= $form['username']->getData();
-				$email 		= $form['email']->getData();
-				$password 	= $form['password']->getData();
-				
-				// create new user
-				$newUser = $this->get("reaccion_cms.user")->createNewUserIfAvailable($username, $email, $password);
-
-				if($newUser === true)
-				{
-					// sign in new user
-					$this->get("reaccion_cms.authentication")->setUser($newUser)->authenticate(true);
-
-					// redirect to home page
-					return $this->redirectToRoute("index");
-				}
-			}
-
-			// view
-			$view = $this->get("reaccion_cms.theme")->getConfigView("register", true);
-			$vars = [
-				'language' 	=> 'en',
-				'form' 		=> $form->createView(),
-				'seo' 		=> $seo
-			];
-
-			return $this->render($view, $vars);
-		}
-
 		/**
 		 * Login existing user
 		 */
-		public function login(Request $request, TranslatorInterface $translator, EncoderFactoryInterface $encoder)
+		public function index(Request $request, TranslatorInterface $translator, EncoderFactoryInterface $encoder)
 		{
 			// If user is already logged in, redirect to home
 			if( ! empty($this->getUser()) )
---
+++ Controller/User/RegistrationController.php
@@ -0,0 +1,61 @@
+<?php
+
+	namespace App\ReaccionEstudio\ReaccionCMSBundle\Controller\User;
+
+	use Symfony\Component\HttpFoundation\Request;
+	use Symfony\Component\Translation\TranslatorInterface;
+	use Symfony\Component\HttpFoundation\RedirectResponse;
+	use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+	use App\ReaccionEstudio\ReaccionCMSBundle\Constants\Cookies;
+	use App\ReaccionEstudio\ReaccionCMSBundle\Form\Users\UserLoginType;
+	use App\ReaccionEstudio\ReaccionCMSBundle\Form\Users\UserRegisterType;
+	use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
+	use Symfony\Component\Security\Http\Event\InteractiveLoginEvent;
+	use Symfony\Component\Security\Core\Encoder\EncoderFactoryInterface;
+
+	class RegistrationController extends Controller
+	{
+		/**
+		 * Register a new user
+		 */
+		public function index(Request $request, TranslatorInterface $translator)
+		{
+			$sitename = $this->get("reaccion_cms.config")->get("site_name");
+			$seo = [
+				'title' => $translator->trans("user_register.seo_title", ['%sitename%' => $sitename])
+			];
+
+			// form
+			$form = $this->createForm(UserRegisterType::class);
+			$form->handleRequest($request);
+
+			if ($form->isSubmitted() && $form->isValid()) 
+			{
+				$username 	= $form['username']->getData();
+				$email 		= $form['email']->getData();
+				$password 	= $form['password']->getData();
+				
+				// create new user
+				$newUser = $this->get("reaccion_cms.user")->createNewUserIfAvailable($username, $email, $password);
+
+				if($newUser === true)
+				{
+					// sign in new user
+					$this->get("reaccion_cms.authentication")->setUser($newUser)->authenticate(true);
+
+					// redirect to home page
+					return $this->redirectToRoute("index");
+				}
+			}
+
+			// view
+			$view = $this->get("reaccion_cms.theme")->getConfigView("register", true);
+			$vars = [
+				'language' 	=> 'en',
+				'form' 		=> $form->createView(),
+				'seo' 		=> $seo
+			];
+
+			return $this->render($view, $vars);
+		}
+	}
