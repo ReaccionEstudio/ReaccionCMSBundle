10489017b267ea634be2bb5cc6f3f3b4efc48d7c
viher3 <viher_3@hotmail.com>
2019-02-04 17:25:26 +0100

    Fixed error when loading the CMS welcome page

---
+++ Constants/ReaccionCMS.php
@@ -0,0 +1,11 @@
+<?php
+	
+	namespace ReaccionEstudio\ReaccionCMSBundle\Constants;
+
+	/**
+	 * Reaccion CMS constants
+	 */
+	class ReaccionCMS
+	{
+		const VERSION = "0.1.4-alpha";
+	}
--- Repository/PageRepository.php
+++ Repository/PageRepository.php
@@ -7,4 +7,22 @@ use ReaccionEstudio\ReaccionCMSBundle\Entity\Page;
 
 class PageRepository extends EntityRepository
 {
+	/**
+	 * Get total Page entities
+	 *
+	 * @return Interger 	[type] 		Total existing page entities
+	 */
+	public function getTotalPages() : Int
+	{
+		$em = $this->getEntityManager();
+
+    	$dql = "SELECT COUNT(p.id) AS total 
+                FROM ReaccionEstudio\ReaccionCMSBundle\Entity\Page p 
+                WHERE p.isEnabled = 1";
+
+        $query  = $em->createQuery($dql);
+    	$result = $query->getSingleResult();
+
+    	return ($result['total']) ?? 0;
+	}
 }
