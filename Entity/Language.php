888e6c1ac6586c562558613486413555967f7d1d
viher3 <viher_3@hotmail.com>
2019-03-31 18:57:29 +0200

    Added language entity getters and setters

--- Entity/Language.php
+++ Entity/Language.php
@@ -49,5 +49,84 @@ class Language
      */
     private $position;
 
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     */
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getFlag(): ?string
+    {
+        return $this->flag;
+    }
+
+    /**
+     * @param string|null $flag
+     */
+    public function setFlag(?string $flag): void
+    {
+        $this->flag = $flag;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isEnabled(): ?bool
+    {
+        return $this->enabled;
+    }
+
+    /**
+     * @param bool $enabled
+     */
+    public function setEnabled(bool $enabled): void
+    {
+        $this->enabled = $enabled;
+    }
+
+    /**
+     * @return int
+     */
+    public function getPosition(): ?int
+    {
+        return $this->position;
+    }
+
+    /**
+     * @param int $position
+     */
+    public function setPosition(int $position): void
+    {
+        $this->position = $position;
+    }
 
 }
