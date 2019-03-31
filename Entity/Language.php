6c438fc90cd14b8c2951bf16fba43c8512732504
viher3 <viher_3@hotmail.com>
2019-03-31 16:49:29 +0200

    Language entity

---
+++ Entity/Language.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace ReaccionEstudio\ReaccionCMSBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Language
+ *
+ * @ORM\Table(name="language")
+ * @ORM\Entity(repositoryClass="ReaccionEstudio\ReaccionCMSBundle\Repository\LanguageRepository")
+ */
+class Language
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @var string|null
+     *
+     * @ORM\Column(name="flag", type="string", length=50, nullable=true)
+     */
+    private $flag;
+
+    /**
+     * @var bool
+     *
+     * @ORM\Column(name="enabled", type="boolean")
+     */
+    private $enabled;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="position", type="integer")
+     */
+    private $position;
+
+
+}
---
+++ Repository/LanguageRepository.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace ReaccionEstudio\ReaccionCMSBundle\Repository;
+
+use Doctrine\ORM\EntityRepository;
+use ReaccionEstudio\ReaccionCMSBundle\Entity\Language;
+
+class LanguageRepository extends EntityRepository
+{
+
+}
