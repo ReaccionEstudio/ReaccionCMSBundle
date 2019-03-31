3de68d16152990f1ae6b5382f53f9864db5a118f
viher3 <viher_3@hotmail.com>
2019-03-31 14:31:25 +0200

    Fixed fos_js_routes.json file relative path

--- Resources/public/javascript/RoutingManager.js
+++ Resources/public/javascript/RoutingManager.js
@@ -1,6 +1,12 @@
 // fos-js-routing
-const routes = require('../../../../../public/js/fos_js_routes.json');
-import Routing from '../../../../../vendor/friendsofsymfony/jsrouting-bundle/Resources/public/js/router.min.js';
+/**
+ * Use this relative route for develop:
+ *
+ *	'../../../../../public/js/fos_js_routes.json'
+ * 	'../../../../../vendor/friendsofsymfony/jsrouting-bundle/Resources/public/js/router.min.js'
+ */
+const routes = require('../../../../../../public/js/fos_js_routes.json');
+import Routing from '../../../../../../vendor/friendsofsymfony/jsrouting-bundle/Resources/public/js/router.min.js';
 
 Routing.setRoutingData(routes);
 
