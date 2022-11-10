0fc7894ae56e39c4f837134056698f22549a8465
viher3 <viher_3@hotmail.com>
2022-11-10 19:01:40 +0100

    Disable LanguageListener

--- EventListener/LanguageListener.php
+++ EventListener/LanguageListener.php
@@ -1,32 +1,34 @@
 <?php
 
-	namespace ReaccionEstudio\ReaccionCMSBundle\EventListener;
+namespace ReaccionEstudio\ReaccionCMSBundle\EventListener;
 
-	use Symfony\Component\HttpFoundation\Request;
-	use Symfony\Component\HttpKernel\Event\GetResponseEvent;
-    use ReaccionEstudio\ReaccionCMSBundle\Services\Language\LanguageService;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+use ReaccionEstudio\ReaccionCMSBundle\Services\Language\LanguageService;
 
-    /**
-     * Language|Locale listener
-     *
-     * @author Alberto Vian <alberto@reaccionestudio.com>
-     */
-	class LanguageListener
-	{
-        public function __construct(LanguageService $language)
-        {
-            $this->language = $language;
-        }
+/**
+ * Language|Locale listener
+ *
+ * @author Alberto Vian <alberto@reaccionestudio.com>
+ */
+class LanguageListener
+{
+    public function __construct(LanguageService $language)
+    {
+        $this->language = $language;
+    }
 
-	    public function onKernelRequest(GetResponseEvent $event)
-	    {
-    		$request  = $event->getRequest();
-            $route = $request->get('_route');
+    /*
+    public function onKernelRequest(GetResponseEvent $event)
+    {
+        $request  = $event->getRequest();
+        $route = $request->get('_route');
 
-            // get locale
-            $locale = $this->language->getLanguage();
+        // get locale
+        $locale = $this->language->getLanguage();
 
-            // set app locale
-    		$request->setLocale($locale);
-	    }
-	}
+        // set app locale
+        $request->setLocale($locale);
+    }
+    */
+}
